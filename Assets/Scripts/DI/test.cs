30559801dca0c4d965b15cf93a9d45fa595e382f
Hard_Code <zero-0_0@bk.ru>
2024-05-16 18:54:22 +0500

    test scripts in unity

---
+++ Assets/Scripts/DI/test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+
+    
+    private  void Update()
+    {
+             
+    }   
+}
