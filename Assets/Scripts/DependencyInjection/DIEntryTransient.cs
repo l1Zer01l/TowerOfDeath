5307537ff4f8b544a8dc2d3c16ea1ebc1b48143f
Hard_Code <zero-0_0@bk.ru>
2024-05-17 22:01:47 +0500

    Impementation DIEntryTransient

--- Assets/Scripts/DependencyInjection/DIEntryTransient.cs
+++ Assets/Scripts/DependencyInjection/DIEntryTransient.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Collections;
-
 
 namespace TowerOfDeath.DI
 {
     public class DIEntryTransient<T> : DIEntry<T>
     {
-        public DIEntryTransient(DIContainer container, Func<DIContainer, T> factory) : base(container, factory)
-        {
-        }
+        public DIEntryTransient(DIContainer container, Func<DIContainer, T> factory) : base(container, factory) { }
 
         public override T Resolve()
         {
-            throw new System.NotImplementedException();
+            return _factory(_container);
         }
     }
 }
