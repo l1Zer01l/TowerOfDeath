153c4ba15faae57eae973154e4a1e2264573ad7d
Hard_Code <zero-0_0@bk.ru>
2024-05-17 21:48:56 +0500

    added DI Entry for Dependency Injection

---
+++ Assets/Scripts/DependencyInjection/DIContainer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerOfDeath.DI
+{
+    public class DIContainer
+    {
+        private DIContainer _parentContainer;
+
+    }
+}
---
+++ Assets/Scripts/DependencyInjection/DIEntry.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace TowerOfDeath.DI
+{
+    public abstract class DIEntry
+    {
+        protected DIContainer _container { get; private set; }
+        public DIEntry(DIContainer container)
+        {
+            _container = container;
+        }
+    }
+
+    public abstract class DIEntry<T> : DIEntry
+    {
+        protected Func<DIContainer, T> _factory { get; private set; }
+
+        public DIEntry(DIContainer container, Func<DIContainer, T> factory) : base(container)
+        {
+            _factory = factory;
+        }
+        public abstract T Resolve();
+
+    }
+}
