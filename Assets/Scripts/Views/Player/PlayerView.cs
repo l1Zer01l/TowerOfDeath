503801212af1cef3892a31e6d23ea118d8660c1b
Hard_Code <zero-0_0@bk.ru>
2024-05-30 17:41:36 +0500

    added PoolObject

--- Assets/Scripts/Views/Player/PlayerView.cs
+++ Assets/Scripts/Views/Player/PlayerView.cs
@@ -8,8 +8,10 @@ namespace TowerOfDeath
         private const string ANIMATION_Y = "y";
         private const string ANIMATION_LAST_X = "lastX";
         private const string ANIMATION_LAST_Y = "lastY";
+
         private Animator _animator;
         private Vector3 _directionLast = Vector3.zero;
+
         public void Start()
         {
             _animator = GetComponent<Animator>();
@@ -27,12 +29,15 @@ namespace TowerOfDeath
             if (direction.magnitude != 0)
                 _directionLast = direction;
 
+            //TODO: implementation animation player in FSM state machine 
+
             _animator.SetFloat(ANIMATION_X, direction.x);
             _animator.SetFloat(ANIMATION_Y, direction.y);
             _animator.SetFloat(ANIMATION_LAST_X, _directionLast.x);
             _animator.SetFloat(ANIMATION_LAST_Y, _directionLast.y);
 
             transform.position += direction * speed * Time.deltaTime;
+            
         }
     }
 }
